25842275ccd5e0301e0f2ef777d584853c731d04
Krmogi <75190777+KrishayR@users.noreply.github.com>
2021-02-06 21:07:24 -0800

    Update settings.py

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -19,11 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ycdcbdeq66gbqe!2&*3$(yua4o_dvtu*ll=c_rp(+-ctg3fb7)'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
